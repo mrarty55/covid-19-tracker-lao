a7312b4d167a836f812d9d164c1929febf4af23a
Arty <atyinthavongsa@gmail.com>
2020-03-31 00:21:32 +0700

    added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        {{ year }} © COVID-19 Tracker Website. Built with ❤️ by Philaphonh
+        Inthavongsa
+      </p>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: "Foot",
+  data: () => {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
